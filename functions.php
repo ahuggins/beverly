1e8bd7f1e79c5ec5fd7c76b2aa64e6b0881748c3
Andy Huggins <andrewhuggins@gmail.com>
2014-10-31 08:09:43 -0400

    updating the title tag to display better with a filter in functions.php

--- functions.php
+++ functions.php
@@ -4,3 +4,35 @@ add_action( 'after_setup_theme', 'register_my_menu' );
 function register_my_menu() {
   register_nav_menu( 'primary', 'Primary Menu' );
 }
+
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string The filtered title.
+ */
+function theme_name_wp_title( $title, $sep ) {
+	if ( is_feed() ) {
+		return $title;
+	}
+	
+	global $page, $paged;
+
+	// Add the blog name
+	$title .= get_bloginfo( 'name', 'display' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title .= " $sep $site_description";
+	}
+
+	// Add a page number if necessary:
+	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+		$title .= " $sep " . sprintf( __( 'Page %s', '_s' ), max( $paged, $page ) );
+	}
+
+	return $title;
+}
+add_filter( 'wp_title', 'theme_name_wp_title', 10, 2 );
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 	    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
-	  	<title><?php wp_title(); ?></title>
+	  	<title><?php wp_title('|', true, 'right'); ?></title>
 		<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" type="text/css">
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 		<?php wp_head(); ?>
